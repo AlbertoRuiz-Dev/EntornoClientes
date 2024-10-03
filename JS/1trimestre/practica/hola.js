3804de5423e763b7da432e67bb90f580f649dcb6
Alberto Ruiz Díaz <aruidia3008@g.educaand.es>
2024-10-03 11:43:31 +0200

    Primer commit

---
+++ JS/1trimestre/practica/hola.js
@@ -0,0 +1,17 @@
+function funcion() {
+  document.getElementById("demo").innerHTML = Date();
+}
+
+let x = "2" + 5 + 2;
+let fruits = ["Banana", "Orange", "Apple"];
+
+fruits = ["Hola", "Adios"];
+let txt = "";
+for (let l in fruits) {
+  txt += fruits[l] + " ";
+}
+
+function fun(nombre) {
+  console.log("Hola " + nombre);
+}
+

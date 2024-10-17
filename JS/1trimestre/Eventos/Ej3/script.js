ddada6b5c530c7b76431cd3bd5bd0993c3c7a771
Alberto Ruiz Díaz <aruidia3008@g.educaand.es>
2024-10-17 12:33:52 +0200

    17/10/24

--- JS/1trimestre/Eventos/Ej3/script.js
+++ JS/1trimestre/Eventos/Ej3/script.js
@@ -4,14 +4,23 @@ function agregar(){
     let input = document.getElementById("newItemInput");
     if (input.value!=""){
     let ul = document.getElementById("itemsList");
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    //botones
     let botonBorrar = li.appendChild(document.createElement("img"));
     botonBorrar.classList.add('deleteBtn');
     botonBorrar.src = "https://cdn-icons-png.flaticon.com/512/58/58326.png";
     let botoncomp = li.appendChild(document.createElement("img"));
     botoncomp.classList.add('completeBtn');
-    
+    botoncomp.src= "pngegg.png"
+    //eventListener 
+    botoncomp.addEventListener("click", () => {
+        li.style.textDecoration = "line-through"; // Tachamos el texto al marcarlo como completado
+    });
+    botonBorrar.addEventListener("click",()=>{
+        li.remove()
+    });
+
     ul.appendChild(li);
     input.value = "";
 }else{
